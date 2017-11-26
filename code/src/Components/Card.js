3a4ac9d007a481dd0c83bdf2503e4eca573f770b
torresNicolasMauro <torres.nicolas.mauro@gmail.com>
2017-11-26 14:14:23 -0300

    cards visually ok, open post functional

--- code/src/Components/Card.js
+++ code/src/Components/Card.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
  class Card extends Component {
   render() {
     return (
-        <div className="card">
-         <img className="card-img-top" src="https://dummyimage.com/600x400/000/fff" alt="pepe"/>
+        <div className="card" id = { this.props.id }>
+         <img className="card-img-top" src={this.props.img} alt="pepe"/>
           <div className="card-body">
-            <h4 className="card-title">Card title</h4>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h4 className="card-title">{this.props.title}</h4>
+            <p className="card-text">{this.props.description}</p>
           </div>
          </div>
     );
--- code/src/Components/DashBoard.js
+++ code/src/Components/DashBoard.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Card from './Card';
+import cards from '../Assests/cards';
 
 class DashBoard extends Component {
+    constructor(props){
+        super(props);
+
+        this._openPost = this._openPost.bind(this);
+    }
+
+    _openPost(event){
+        event.preventDefault();
+        this.props.history.push('/post?id='+event.target.id); //redirect you
+    }
+
     render() {
         var cols = [];
-        var sizeCards = 10;
-       
-        for (var i = 0; i <sizeCards; i++){
-            cols.push(<Col xs className="box" key={i}> <Card /> </Col>);
+        for (var i = 0; i <cards.length; i++){
+            cols.push(  <Col xs  //auto size cards
+                             className="box" 
+                             onClick = { this._openPost } 
+                             key = { cards[i].cardId } 
+                            > 
+                              <Card img = { cards[i].cardImageUrl } 
+                                    title = { cards[i].cardTitle } 
+                                    description = { cards[i].cardDescription }
+                                    id = { cards[i].cardId } /> 
+                        </Col>);
         }
 
         return (
